8306e35a323f2fc40478fdd290f8702c592714b9
httpgate <xinjingjie.us@gmail.com>
2025-05-30 00:51:01 -0400

    minor update

--- replace.js
+++ replace.js
@@ -15,8 +15,8 @@ const replace = (searchStr, replaceWith) => {
 		throw new TypeError('replaceWith must be a string.')
 	}
 
-    const searchBuffer = Buffer.from(searchStr);
-    const replaceBuffer = Buffer.from(replaceWith);
+	const searchBuffer = Buffer.from(searchStr);
+	const replaceBuffer = Buffer.from(replaceWith);
 
 	// The string data that we aren't yet sure it's part of the search string or not
 	// We have to hold on to this until we are sure.
@@ -25,45 +25,45 @@ const replace = (searchStr, replaceWith) => {
 	var ts = new Transform();
 
 	ts._transform = (chunk, encoding, callback) => {
-        let current = 0;
-        let next = 0;
+		let current = 0;
+		let next = 0;
 
-        if(unsureBuffer.length){
-            chunk = Buffer.concat([unsureBuffer,chunk]);
-            unsureBuffer ='';
-        }
+		if(unsureBuffer.length){
+			chunk = Buffer.concat([unsureBuffer,chunk]);
+			unsureBuffer ='';
+		}
 
-        if(chunk.length < searchBuffer.length){
-            unsureBuffer = chunk;
-            return callback();
-        }
-        
-        while(next = chunk.indexOf(searchBuffer,current)){
+		if(chunk.length < searchBuffer.length){
+			unsureBuffer = chunk;
+			return callback();
+		}
+		
+		while(next = chunk.indexOf(searchBuffer,current)){
 
-            if(next>0 && next + searchBuffer.length > chunk.length){
-                unsureBuffer = chunk.slice(next);
-                ts.push(chunk.slice(current, next));
-                ts.push(replaceBuffer);
-                break;
-            }
-            else if(next===-1){
-                next = Math.max(current, chunk.length - searchBuffer.length + 1);
-                unsureBuffer = chunk.slice(next);
-                ts.push(chunk.slice(current,next));
-                break;
-            }
-            else {
-                ts.push(chunk.slice(current,next));
-                ts.push(replaceBuffer);
-                current = next + searchBuffer.length;
-            }
-        }
-        callback();
+			if(next>0 && next + searchBuffer.length > chunk.length){
+				unsureBuffer = chunk.slice(next);
+				ts.push(chunk.slice(current, next));
+				ts.push(replaceBuffer);
+				break;
+			}
+			else if(next===-1){
+				next = Math.max(current, chunk.length - searchBuffer.length + 1);
+				unsureBuffer = chunk.slice(next);
+				ts.push(chunk.slice(current,next));
+				break;
+			}
+			else {
+				ts.push(chunk.slice(current,next));
+				ts.push(replaceBuffer);
+				current = next + searchBuffer.length;
+			}
+		}
+		callback();
 	}
 
 	ts._flush = (callback) => {
-        ts.push(unsureBuffer);
-        callback()
+		ts.push(unsureBuffer);
+		callback()
 	}
 
 	return ts;
